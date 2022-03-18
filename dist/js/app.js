ac94c372e12549714852f2ca32008eb8de3ae50e
Liang <>
2022-03-18 21:57:39 +0800

    Fix: upload dist

---
+++ dist/js/app.js
@@ -0,0 +1,1 @@
+"use strict";$("button.hamburger.hamburger--collapse").on("click",function(){$("aside.menu_aside").toggleClass("-open"),$("button.hamburger.hamburger--collapse").toggleClass("is-active")});

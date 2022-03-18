e19bf3584aa257f55f3b290e92010731ca6bad1d
Liang <>
2022-03-18 22:19:42 +0800

    delete dist

--- dist/js/app.js
+++
@@ -1,1 +0,0 @@
-"use strict";$("button.hamburger.hamburger--collapse").on("click",function(){$("aside.menu_aside").toggleClass("-open"),$("button.hamburger.hamburger--collapse").toggleClass("is-active")});
